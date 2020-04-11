feafdf10191cbaca13b5d534dba7953407f0b94a
Grant Gibson <grantgib@umich.edu>
2020-04-11 15:51:37 -0400

    Finished outer approximation work/plots

--- Code/Plot_Results.m
+++ Code/Plot_Results.m
@@ -8,18 +8,23 @@ sol_1step = load('1step_outer');
 V_0step = sol_0step.V_opt;
 V_1step = sol_1step.V_opt;
 
-% Plots
+V_1max = double(subs(V_1step,[x1,x2,t],[0,0,0]))
+
+%% Plots
 figure;
+% fs0 = fsurf(V_0step + V_1max,'EdgeColor','none');
 fs0 = fsurf(V_0step,'EdgeColor','none');
-fs0.FaceColor = 'b';
+fs0.FaceColor = 'k';
+fs0.FaceAlpha = 0.5;
 hold on;
 fs1 = fsurf(subs(V_1step,t,0), 'EdgeColor','none');
-fs1.FaceColor = 'y';
+fs1.FaceColor = [0.9290, 0.6940, 0.1250];
 fs1.FaceAlpha = 0.5;
-xlim([-0.5 0.5]); xlabel("$x_{cm}$",'interpreter','latex');
-ylim([-1 1]); ylabel("$\dot{x}_{cm}$",'interpreter','latex');
-zlim([0 inf]); zlabel("V^*");
 
+sz = 25;
+xlim([-0.5 0.5]); xlabel("$x_{cm}$",'interpreter','latex','FontSize',sz);
+ylim([-1 1]); ylabel("$\dot{x}_{cm}$",'interpreter','latex','FontSize',sz); 
+zlim([0 inf]); zlabel("V^*",'FontSize',sz/2); 
 
 
 
